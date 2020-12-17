7f4cfe3ab3613543b80bacbae5349f9fa65a6871
Bikorimana Patrick <patrick.bikorimana@student.vinci.be>
2020-12-17 14:03:28 +0100

    fix - refactor of the assertEquals

---
+++ tests/PositivityControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+require "vendor/autoload.php";
+require "controllers/PositivityController.php";
+
+class PositivityControllerTest extends TestCase
+{
+
+    $this->assertEquals("test","test");
+
+}
