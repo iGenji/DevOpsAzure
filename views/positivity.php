c19e71f38a9df107a198d377e8053917bca8f308
Bikorimana Patrick <patrick.bikorimana@student.vinci.be>
2020-12-17 15:51:19 +0100

    fix - fix view positivity.php by fixing the index.php

--- views/positivity.php
+++ views/positivity.php
@@ -1,4 +1,16 @@
 <div id="contenu">
 	<h2>Positivité</h2>
 	<p>Bienvenue sur la page de la positivité !</p>
+    <div id="contenu">
+
+        <p>Entrez votre message de bonheur ici, chaque mot doit être séparé par une virgule ex: Content,Parfait,etc.</p>
+        <div id="notification"><?php echo $notification ?></div>
+        <div class="formulaire">
+            <form action="index.php?action=contact" method="post">
+
+                <p><textarea rows="3" name="message">Entrez vos mots</textarea></p>
+                <p><input type="submit" name="form_contact" value="Envoyer"></p>
+            </form>
+        </div>
+    </div><!-- #contenu -->
 </div><!-- #contenu -->
